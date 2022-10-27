f2829aa3748e824fc69341acd3993ceb3ef38fa4
JimStudio <chuixiaojian@hotmail.com>
2022-10-28 00:54:48 +0800

    new

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import './App.css';
+import Navbar from './Navbar';
 
 function App() {
-    const title='Welcom to the new blog';
-    const likes =50;
-    //const person = {name: 'yoshi', age: 30};
+
   return (
     <div className="App">
+      <Navbar/>
       <div className="content">
-         <h1>{title}t</h1>
-         <p>Liked {likes} times</p>
-         {/* <p>{person}</p> */}
       </div>
     </div>
   );
