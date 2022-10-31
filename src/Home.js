2ad428471ff097dd1523ffc3a09e67f269ea9bea
JimStudio <chuixiaojian@hotmail.com>
2022-11-01 02:57:03 +0800

    new

--- src/Home.js
+++ src/Home.js
@@ -1,24 +1,30 @@
 import { useState } from "react";
+import Bloglist from "./Bloglist";
 
 const Home = () => {
 
-  //let name = 'mario';
-  const [name, setName] = useState('mario');
-  const [age, setage] = useState(25);
 
-  const handleClick = () => {
-    setName('lugi');
-    setage(30);
-  }
 
-      
+    const [blogs, setblogs] = useState([
+      { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+      { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+      { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+    ]);
     return (
       <div className="home">
-         <h2>Home Page</h2>
-         <p> { name } is { age } years old</p>
-         <button onClick={handleClick}>Click Me</button>
+         <Bloglist blogs={blogs}/>
       </div>
     );
 }
  
 export default Home;
+
+  //let name = 'mario';
+  // const [name, setName] = useState('mario');
+  // const [age, setage] = useState(25);
+  // const handleClick = () => {
+  //   setName('lugi');
+  //   setage(30);
+  // }
+{/* <p> { name } is { age } years old</p>
+         <button onClick={handleClick}>Click Me</button> */}
