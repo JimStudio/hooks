caf7e7f2be4ac36298f0f356ae12d8044cfae04f
JimStudio <chuixiaojian@hotmail.com>
2022-11-02 02:20:51 +0800

    new

--- src/Home.js
+++ src/Home.js
@@ -12,7 +12,8 @@ const Home = () => {
     ]);
     return (
       <div className="home">
-         <Bloglist blogs={blogs}/>
+         <Bloglist blogs={blogs} title="All Blogs!"/>
+         <Bloglist blogs={blogs.filter((blog) => blog.author === "mario" )} title="Mario's blos"/>
       </div>
     );
 }
