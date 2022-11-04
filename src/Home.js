845f3b5e68553b627aaf0b6f1c076845b4132a20
JimStudio <chuixiaojian@hotmail.com>
2022-11-05 03:23:39 +0800

    new

--- src/Home.js
+++ src/Home.js
@@ -1,21 +1,28 @@
-import { useState } from "react";
+import { useState, useEffect} from "react";
 import Bloglist from "./Bloglist";
 
 const Home = () => {
-
-
-
     const [blogs, setblogs] = useState([
       { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
       { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
       { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
     ]);
+
+    const handleDelete =(id) =>{
+        const newBlogs = blogs.filter(blog => blog.id != id);
+        setblogs(newBlogs);
+    }
+
+    useEffect(() => {
+      
+    });
     return (
       <div className="home">
-         <Bloglist blogs={blogs} title="All Blogs!"/>
-         <Bloglist blogs={blogs.filter((blog) => blog.author === "mario" )} title="Mario's blos"/>
+         <Bloglist blogs={blogs} title="All Blogs!" handleDelete={handleDelete} />
+        {/* // <Bloglist blogs={blogs.filter((blog) => blog.author === "mario" )} title="Mario's blos"/> */}
       </div>
     );
+    
 }
  
 export default Home;
