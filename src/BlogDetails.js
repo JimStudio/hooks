c9dda55b4e3070b291e88dbcdcb4d1aaab4ec69f
Your Name <you@example.com>
2022-12-17 20:53:38 +0800

    new

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,7 +1,9 @@
 import { useParams } from "react-router-dom";
+import useFetch from './useFetch';
 
 const BlogDetalis = () => {
     const {id} = useParams();
+    const {data, error, isPending} = useFetch('http://localhost:8000/blogs/');
     return (  
         <div className="blog-detalis">
             <h2>Blog Detalis - { id }</h2>
