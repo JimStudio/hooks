3bc25a4ec443794a40fa37ba17716ee53b0dbf26
JimStudio <chuixiaojian@hotmail.com>
2022-12-21 03:10:33 +0800

    new

--- src/Create.js
+++ src/Create.js
@@ -5,11 +5,29 @@ const Create = () => {
     const [title, setTitle] = useState('');
     const [body, setBody] = useState('');
     const [author, setAuhor] = useState('mario');
+    const [isPending, setisPending] = useState(false)
     
+    const handleSumbit =  (e) => {
+        e.preventDefault();
+        const blog = {title, body, author};
+
+        setisPending(true);
+
+        fetch('http://localhost:8000/blogs', {
+            method : 'POST',
+            headers:{"Content-Type": "application/json"},
+            body: JSON.stringify(blog),
+
+        }).then(() => {
+            console.log('new blog');
+            setisPending(false);
+        })
+    }
+
     return (
         <div className="create">
             <h2>Add a New Blog</h2>
-            <form action="">
+            <form onSubmit={handleSumbit}>
             <label>Blog title:</label>
             <input
                type="text"
@@ -31,10 +49,8 @@ const Create = () => {
                 <option value="mario">mario</option>
                 <option value="yoshi">yoshi</option>
             </select>
-            <button>Add Blog</button>
-            <p> {title} </p>
-            <p> {body} </p>
-            <p> {author} </p>
+            {!isPending && <button>Add Blog</button>}
+            {isPending && <button disabled>Addinf blog....</button>}
             </form>
         </div>
     );
